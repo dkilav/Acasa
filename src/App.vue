efac0fdc2d684da757aea12c69a34b69f5442025
dkilav <vlad5222vd@gmail.com>
2023-10-05 16:48:08 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+<div>
+   <h1 class="text-3xl text-red-500 text-center italic font-bold underline">
+    Hello world!
+   
+  </h1>
+   <img src="/img/2.png" alt="">
+   
+</div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+ 
+</style>
